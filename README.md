06002d0ef2caa38a0f3926aef10fff7edc31192a
CodingKayla <kaylaspacequeen@gmail.com>
2022-07-10 17:23:29 -0400

    rounding out get_spells and adding readme text

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# R20P
+# Roll20 Parser
+
+*Work in Progress*
+
+Generate a JSON document containing the character information scraped from a Roll20 Character sheet.
+
+* Requires downloading the rendered sheet HTML from Roll20.
+* Currently accomplished by setting the sheet to pop-out into a separate window and using the browser's dev tools to grab the rendered sheet. 
--- r20p.py
+++ r20p.py
@@ -9,31 +9,29 @@ from bs4 import BeautifulSoup
 class Feat:
     name: str
     desc: str
-    source_name: str
-    source_type: str
-    
-@dataclass
-class Feats:
-    feats: List[Feat]
+    sourceName: str
+    sourceType: str
 
 @dataclass
 class Spell:
     name: str
+    spellLevel: str
+    spellSchool: str
+    castingTime: str
+    range: str
+    target: str
+    components: str
+    desc: str
+    higherLevels: str
     
-@dataclass
-class Spells:
-    spells: List[Spell]
-
 @dataclass
 class Character:
-    feats: Feats
-    spells: Spells
+    name: str
+    feats: List[Feat]
+    spells: List[Spell]
     
-def get_feats():
-    feat_list = Feats([])
-    
-    with open("sheet.html") as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
+def get_feats(soup):
+    feat_list = []
         
     repitems = soup.find_all("div", class_="repitem")
 
@@ -54,18 +52,15 @@ def get_feats():
                 feat_attrs = [name, desc, source_name, source_type]
                 
                 if all(x is not None for x in feat_attrs):
-                    feat_list.feats.append(Feat(name.text,
-                                                desc.text,
-                                                source_name.text,
-                                                source_type.text))
+                    feat_list.append(Feat(name.text,
+                                          desc.text,
+                                          source_name.text,
+                                          source_type.text))
     return feat_list
 
-def get_spells():
-    spell_list = Spells([])
+def get_spells(soup):
+    spell_list = []
     
-    with open("sheet.html") as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
-        
     spells = soup.find_all("div", {'class': 'spell'})
     
     for spell in spells:
@@ -73,15 +68,43 @@ def get_spells():
                                    'name': 'attr_spellname'})
         
         if name.text != "":
-            spell_list.spells.append(Spell(name.text))
+            level = spell.find("span", {'name': 'attr_spelllevel'})
+            school = spell.find("span", {'name': 'attr_spellschool'})
+            casting_time = spell.find("span", {'name': 'attr_spellcastingtime'})
+            range = spell.find("span", {'name': 'attr_spellrange'})
+            target = spell.find("span", {'name': 'attr_spelltarget'})
+            components = spell.find("span", {'name': 'attr_spellcomp_materials'})
+            desc = spell.find("span", {'name': 'attr_spelldescription'})
+            higher_levels = spell.find("span", {'name': 'attr_spellathigherlevels'})
+            
+            
+            spell_list.append(Spell(name.text,
+                                    level.text,
+                                    school.text,
+                                    casting_time.text,
+                                    range.text,
+                                    target.text,
+                                    components.text,
+                                    desc.text,
+                                    higher_levels.text))
 
     return spell_list
 
-def main():
-    feats = get_feats()
-    spells = get_spells()
+def get_name(soup):
+    return soup.find("title").text
+
+def build_character(soup):
+    name = get_name(soup)
+    feats = get_feats(soup)
+    spells = get_spells(soup)
     
-    character = Character(feats, spells)
+    return Character(name, feats, spells)
+
+def main():
+    with open("sheet.html") as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+        
+    character = build_character(soup)
     
     print(json.dumps(dataclasses.asdict(character)))
 
